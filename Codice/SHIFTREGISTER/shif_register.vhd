e569158dcf167e189e76c26366036c6ac7e9434c
Falino <alessandrofalino@gmail.com>
2023-10-27 18:39:58 +0200

    Implementato Es 4.1

--- Codice/SHIFTREGISTER/shif_register.vhd
+++ Codice/SHIFTREGISTER/shif_register.vhd
@@ -1,15 +1,17 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
-library ieee;
-use ieee.std_logic_1164.all;
-
 entity shift_register is
-    port(CLK, RST, SI : in std_logic;
-         SO : out std_logic);
+    port(
+        CLK : in std_logic;
+        RST : in std_logic;
+        y_s_r   : in std_logic;
+        i_s_r   : in std_logic;
+        o_s_r   : out std_logic
+        );
 end shift_register;
 
-architecture archi of shift_register is
+architecture shift_registerArch of shift_register is
     signal tmp: std_logic_vector(3 downto 0);
 begin
 
@@ -19,30 +21,16 @@ begin
             if (RST='1') then
                 tmp <= (others=>'0');
             else
-                --questi segnali vengono aggiornati con i valori che avevano alla fine della 
-                -- precedente attivazione
-                tmp(0) <= SI; 
+                tmp(0) <= i_s_r; 
                 tmp(1) <= tmp(0);
                 tmp(2) <= tmp(1);
-                tmp(3) <= tmp(2);
-                                
-                -- SO <= tmp(3);--se inserisco qui questa istruzione invece che fuori dal process,
-                                --l'uscita SO non viene aggiornata contestualmente a tmp(3) 
-                                --ci vorrà un altro fronte del clock per vedere SO e quindi, per conservare
-                                --il valore di tmp(3) fino al prossimo clock, verrà inserito un altro flip-flop
-                                --tra tmp(3) e SO      
+                tmp(3) <= tmp(2);    
             end if;
             
         end if;
-        --SO <= tmp(3); --se inserisco qui questa istruzione, il valore di SO è aggiornato alla prossima
-                        --attivazione del process, quindi sul fronte di discesa successivo a quando è stato
-                        --aggiornato tmp(3)
+
     end process;
-    SO <= tmp(3);   --se inserisco qui questa istruzione, il valore di SO viene aggiornato continuamente
-                    --in maniera concorrente
+    o_s_r <= tmp(3);
     
 
-end archi;
-
-				
-				
+end shift_registerArch;
---
+++ Codice/shift_register_Elaborato/shift_register_tb.vhd
@@ -0,0 +1,91 @@
+library IEEE;
+use IEEE.Std_logic_1164.all;
+use IEEE.Numeric_Std.all;
+
+entity shift_register_tb is
+end;
+
+architecture bench of shift_register_tb is
+
+  component shift_register
+      generic (
+          N: integer := 8
+      );
+      port (
+          CLK, RST, SI: in std_logic;
+          SO: out std_logic;
+          Y: in integer range 1 to 2
+      );
+  end component;
+
+  signal CLK, RST, SI: std_logic;
+  signal SO: std_logic;
+  signal Y: integer range 1 to 2 ;
+
+  constant clock_period: time := 10 ns;
+  signal stop_the_clock: boolean;
+
+    begin
+
+        -- Insert values for generic parameters !!
+        uut: shift_register 
+            generic map ( N   =>  4)
+            port map ( CLK => CLK,
+                       RST => RST,
+                       SI  => SI,
+                       SO  => SO,
+                       Y   => Y );
+
+      
+        stimulus: process
+        begin
+        
+          wait for 100 ns;
+          Y <= '1'; --Cambiare per: Non sovrapposte (0) | Parzialmente sovrapposte (1)
+          SI <= '0';
+         
+          wait for 10 ns;
+          SI <= '1';
+          
+          wait for 10 ns;
+          SI <= '1';
+
+          wait for 10 ns;
+          SI <= '0';
+
+          wait for 10 ns;
+          SI <= '1';
+
+          wait for 10 ns;
+          SI <= '0';
+
+          wait for 10 ns;
+          SI <= '1';
+
+          wait for 10 ns;
+          SI <= '0';
+
+          wait for 10 ns;
+          SI <= '1';
+
+          wait for 10 ns;
+          SI <= '1';
+
+          wait for 10 ns;
+          SI <= '1';
+          
+
+        stop_the_clock <= true;
+        wait;
+    end process;
+
+  clocking: process
+  begin
+    while not stop_the_clock loop
+      CLK <= '0', '1' after clock_period / 2;
+      wait for clock_period;
+    end loop;
+    wait;
+  end process;
+
+end;
